cce2451255331af71ec06fa390dd0365fa92ed9c
Guilherme Costanzi Do <guilhermecostanzi@gmail.com>
2022-06-27 21:45:31 -0300

    Ex 1.

---
+++ ch6-loops/Ex1.py
@@ -0,0 +1,20 @@
+# Programa para determinar os 5 primeiros multiplos de 3 a partir de um número escolhido, maior do que zero.
+
+number = int(input("Digite um número:"))
+
+if number > 0:
+    if number % 3 == 0:
+        end = 4
+        print(number)
+        number = number + 3
+    else:
+        end = 5
+else:
+    end = None
+    print("O programa aceita apenas números maiores que 0.")
+
+for value in range(0, end):
+    while number % 3 != 0:
+        number += 1
+    print(number)
+    number += 3
